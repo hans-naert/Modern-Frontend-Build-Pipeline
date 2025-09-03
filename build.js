82618c96b6e478d8af419dd031f1142e325aa402
Hans Naert <hans.naert@vives.be>
2025-09-03 17:28:48 +0200

    Use build.js to specify options of esbuild

---
+++ build.js
@@ -0,0 +1,22 @@
+// build.js
+const esbuild = require('esbuild');
+
+const isWatch = process.argv.includes('--watch');
+
+const buildOptions = {
+  entryPoints: ['src/main.js'],
+  bundle: true,
+  outfile: 'dist/bundle.js',
+  format: 'esm',
+  loader: { '.svg': 'data-url' },
+  publicPath: 'dist',
+};
+
+if (isWatch) {
+  esbuild.context(buildOptions).then(ctx => {
+    ctx.watch();
+    console.log('Watching for changes...');
+  }).catch(() => process.exit(1));
+} else {
+  esbuild.build(buildOptions).catch(() => process.exit(1));
+}
